09cc6b2084e73984fd73498482755322d302b83f
Max Parzen <max.parzen@ed.ac.uk>
2023-01-27 19:12:39 +0000

    add fixes

--- app.py
+++ app.py
@@ -2,16 +2,15 @@ import streamlit as st
 import pandas as pd
 import plotly.express as px
 
-
 # Load data
 df_storage = pd.read_csv('Data/energy_storage_costs.csv')
 
 # Create plot
 fig_cc = px.scatter_3d(
     df_storage,
-    x='Charger capital costs [EUR/kW]',
-    y='Discharger capital costs [EUR/kW]',
-    z='Store capital costs [EUR/kWh]',
+    x='Charger [EUR/kW]_cc',
+    y='Discharger [EUR/kW]_cc',
+    z='Store [EUR/kWh]_cc',
     size='efficiency',
     color='type',
     symbol='technology',
@@ -20,9 +19,9 @@ fig_cc = px.scatter_3d(
 
 fig_ic = px.scatter_3d(
     df_storage,
-    x='Charger investment costs [EUR/kW]',
-    y='Discharger investment costs [EUR/kW]',
-    z='Store investment costs [EUR/kWh]',
+    x='Charger [EUR/kW]_ic',
+    y='Discharger [EUR/kW]_ic',
+    z='Store [EUR/kWh]_ic',
     size='efficiency',
     color='type',
     symbol='technology',
@@ -30,10 +29,13 @@ fig_ic = px.scatter_3d(
 )
 
 # Plot!
+st.title('19 energy storage technologies by Max Parzen')
 tab1, tab2 = st.tabs(["Capital costs", "Investment costs"])
 with tab1:
     # Capital costs
+    st.title('Capital costs')
     st.plotly_chart(fig_cc, theme="streamlit", use_container_width=True)
 with tab2:
     # Investment costs
+    st.title('Investment costs')
     st.plotly_chart(fig_ic, theme="streamlit", use_container_width=True)
