fdf540badd2757c8277b7b97e44092928ec4fc92
Max Parzen <max.parzen@ed.ac.uk>
2023-02-03 14:09:43 +0000

    add markdown

--- app.py
+++ app.py
@@ -29,13 +29,23 @@ fig_ic = px.scatter_3d(
 )
 
 # Plot!
-st.title('19 energy storage technologies between 2020-2050 by Max Parzen')
-tab1, tab2 = st.tabs(["Capital costs", "Investment costs"])
+st.title('20 energy storage technologies between 2020-2050 by Max Parzen')
+st.markdown(
+    "The total costs of an energy storage is the sum of charger, \n"
+    "discharger and store cost. Instead of lumping these costs together \n"
+    "the 3D image breaks it down. \n\n"
+    "The overnight 'capital costs' are annualised to net present \n"
+    "investment costs with a discount rate over the economic lifetime \n"
+    "using the annuity factor.\n\n"
+    "The capital cost equation is documented here: \n"
+    "https://pypsa-eur.readthedocs.io/en/latest/costs.html#cost-assumptions."
+)
+tab1, tab2 = st.tabs(["Investment costs", "Capital costs"])
 with tab1:
-    # Capital costs
-    st.title('Capital costs')
-    st.plotly_chart(fig_cc, theme="streamlit", use_container_width=True)
-with tab2:
     # Investment costs
     st.title('Investment costs')
     st.plotly_chart(fig_ic, theme="streamlit", use_container_width=True)
+with tab2:
+    # Capital costs
+    st.title('Capital costs')
+    st.plotly_chart(fig_cc, theme="streamlit", use_container_width=True)
