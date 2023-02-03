03371eb8e97bc801606362bf652b5788840e8a29
Max Parzen <max.parzen@ed.ac.uk>
2023-02-03 14:29:44 +0000

    fix text

--- app.py
+++ app.py
@@ -29,16 +29,20 @@ fig_ic = px.scatter_3d(
 )
 
 # Plot!
-st.title('20 energy storage technologies between 2020-2050 by Max Parzen')
+st.title('20 energy storage technologies between 2020-2050')
 st.markdown(
+    "WORK IN PROGRESS. VALIDATION NEEDED. \n\n"
+    
     "The total costs of an energy storage is the sum of charger, \n"
     "discharger and store cost. Instead of lumping these costs together \n"
     "the 3D image breaks it down. The overnight 'capital costs' are annualised to net present \n"
     "investment costs with a discount rate over the economic lifetime \n"
     "using the annuity factor.\n\n"
-    "The capital cost equation is documented here: \n"
-    "https://pypsa-eur.readthedocs.io/en/latest/costs.html#cost-assumptions. \n"
-    "To reproduce the data, checkout: https://github.com/pz-max/energy-storage-data-explorer#energy-storage-data-explorer"
+    "The capital cost equation is documented in [PyPSA](https://pypsa-eur.readthedocs.io/en/latest/costs.html#cost-assumptions) \n"
+    "To reproduce the data, checkout the [GitHub](https://github.com/pz-max/energy-storage-data-explorer#energy-storage-data-explorer) \n"
+    "Goal of the data is to apply the data to energy system models. \n\n"
+    "Reach out to [Max Parzen](https://www.linkedin.com/in/maximilian-parzen-b047a1126/) \n"
+    "for questions or enquiries. Thanks to the [PyPSA community](https://pypsa-meets-earth.github.io/projects.html)! \n "
 )
 tab1, tab2 = st.tabs(["Investment costs", "Capital costs"])
 with tab1:
